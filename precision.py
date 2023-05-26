20352947ae64bdf846a323fb4fe0fa00334813c6
Benoit Savinel <bsavinel@student.42.fr>
2023-05-26 19:33:10 +0200

    UPDATE: function good, next implent comunication

--- precision.py
+++ precision.py
@@ -1,8 +1,9 @@
-print("prediction.py file loading...")
+print("precision.py file loading...")
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import sqrt
 
 #!############################################################################################!#
 #!########################################  Function  ########################################!#
@@ -27,15 +28,10 @@ def add_intercept(x):
 		tmp.resize((tmp.shape[0], 1))
 	return np.insert(tmp, 0, 1, axis=1)
 
-def denormalizer(x, list1):
-	if (not isinstance(x, np.ndarray) or x.size == 0):
+def predict(x, theta):
+	if (not check_matrix(x, -1, 1) or not check_matrix(theta, 2, 1)):
 		return None
-	if (x.ndim != 1 and not (x.ndim == 2 and x.shape[1] == 1)):
-		return None
-	Xcopy = x.reshape(-1)
-	min = np.min(list1)
-	max = np.max(list1)
-	return Xcopy * (max - min) + min
+	return np.dot(add_intercept(x), theta)
 
 def normalizer(x, list1):
 	if (not isinstance(x, np.ndarray) or x.size == 0):
@@ -47,11 +43,58 @@ def normalizer(x, list1):
 	max = np.max(list1)
 	return (Xcopy - min) / (max - min)
 
+def denormalizer(x, list1):
+	if (not isinstance(x, np.ndarray) or x.size == 0):
+		return None
+	if (x.ndim != 1 and not (x.ndim == 2 and x.shape[1] == 1)):
+		return None
+	Xcopy = x.reshape(-1)
+	min = np.min(list1)
+	max = np.max(list1)
+	return Xcopy * (max - min) + min
+
+def mse(y, y_hat):
+	if (not check_matrix(y, -1, -1, 1) or not check_matrix(y_hat, y.shape[0], -1, 1)):
+		return None
+	return sum((y_hat - y) ** 2) / y.size
+
+def rmse(y, y_hat):
+	ret = mse(y, y_hat)
+	if (ret is None):
+		return None
+	return sqrt(ret)
+
+def mae(y, y_hat):
+	if (not check_matrix(y, -1, -1, 1) or not check_matrix(y_hat, y.shape[0], -1, 1)):
+		return None
+	return sum(abs(y_hat - y)) / y.size
+
+def r2score(y, y_hat):
+	if (not check_matrix(y, -1, -1, 1) or not check_matrix(y_hat, y.shape[0], -1, 1)):
+		return None
+	return 1 - (sum((y_hat - y) ** 2) / sum((y - y.mean()) ** 2))
+
 #!####################################################################################################!#
 #!##############################################  TEST  ##############################################!#
 #!####################################################################################################!#
 
 if __name__ == '__main__':
+	for i in range(4):
+		if (i == 3):
+			print("To retry, relaunch the program")
+			exit(1)
+		try:
+			file = str(input("\nFile name: "))
+			if (isinstance(file, str) and file.endswith(".csv")):
+				print("Try to read csv file...")
+				data = pd.read_csv(file)
+				print("csv file is good")
+				break
+			print("Invalid file name, please give a csv file")
+		except:
+			print("Fail to read csv file")
+			pass
+
 	for i in range(4):
 		if (i == 3):
 			print("To retry, relaunch the program")
@@ -64,40 +107,28 @@ if __name__ == '__main__':
 		except:
 			print("Invalid value")
 
-	if (isNormalised == "Y"):
-		for i in range(4):
-			if (i == 3):
-				print("To retry, relaunch the program")
-				exit(1)
-			try:
-				file = str(input("\nFile name: "))
-				if (isinstance(file, str) and file.endswith(".csv")):
-					print("Try to read csv file...")
-					data = pd.read_csv(file)
-					print("csv file is good")
-					break
-				print("Invalid file name, please give a csv file")
-			except:
-				print("Invalid value")
-
 	for i in range(4):
 		if (i == 3):
 			print("To retry, relaunch the program")
 			exit(1)
 		try:
-			theta0 = float(input("\ntheta0: "))
-			theta1 = float(input("theta1: "))
-			value = int(input("value: "))
-			break
+			theta0 = float(input("\ntheta0 for normalized dataset: "))
+			theta1 = float(input("theta1 for normalized dataset: "))
 		except:
 			print("Invalid value")
-
-	if (isNormalised == "N"):
-		print("Result of the prediction : ", theta0 + theta1 * value)
-	else:
-		y = np.array(data[data.columns.values[1]]).reshape(-1, 1)
-		x = np.array(data[data.columns.values[0]]).reshape(-1, 1)
-		value = normalizer(np.array([value]), x)
-		pred = np.array([[theta0 + theta1 * value[0]]])
-		print(pred[0])
-		print("Result of the prediction : ", denormalizer(pred, y)[0])
+	
+	x = np.array(data[data.columns.values[0]]).reshape(-1, 1)
+	y = np.array(data[data.columns.values[1]])
+	xUse = x
+	yUse = y
+	if (isNormalised == "Y"):
+		xUse = normalizer(x, x)
+		yUse = normalizer(y, y)
+	theta = np.array([[theta0],[theta1]])
+	prediction =  predict(x, theta)
+	if (isNormalised == "Y"):
+		prediction = denormalizer(prediction, y)
+	print("\nThe mse of the model :", mse(y, prediction))
+	print("\nThe rmse of the model :", rmse(y, prediction))
+	print("\nThe mae of the model :", mae(y, prediction))
+	print("\nThe r2score of the model :", r2score(y, prediction))
